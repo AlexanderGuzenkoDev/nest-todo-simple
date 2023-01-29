06768de5db475a42e688fa76f7402e16837867e8
dragonwarrior980706 <dragon.warrior980706@gmail.com>
2023-01-29 04:49:59 +0200

    created todo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodoController } from './todo/todo.controller';
+import { TodoService } from './todo/todo.service';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TodoModule, MongooseModule.forRoot('mogongodb://localhost/nest')],
+  controllers: [AppController, TodoController],
+  providers: [AppService, TodoService],
 })
 export class AppModule {}
